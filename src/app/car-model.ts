5c50dc1981f4888978482b4d8ca056815c0eaed5
Vitalii Kulyk <vitcool@ukr.net>
2017-08-07 21:06:41 +0300

    FC_2: added car model and file with car mock data

---
+++ src/app/car-model.ts
@@ -0,0 +1,13 @@
+export class CarModel {
+    id: number;
+    vendor: string;
+    model: string;
+    year: number;
+    VEngine : number;
+    typeOfEngine: string;
+    horsepower: number;
+    color: string;
+    price: number;
+    priceCustomClearance: number;
+    linkToPhoto: string;
+}
